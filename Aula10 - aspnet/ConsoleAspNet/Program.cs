a098ebd77928303694fbf75f8a28abbb8d1d93da
Cassio Costa <cassio.hc@gmail.com>
2022-04-22 21:58:03 -0300

    aula 10

---
+++ Aula10 - aspnet/ConsoleAspNet/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleAspNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .Configure(
+                    app => {
+                        app.Run(context =>
+                         context.Response.WriteAsync("<h1>Hello World<h1>"));
+                    }
+                )
+                .Build();
+            host.Run();
+                
+        }
+    }
+}
