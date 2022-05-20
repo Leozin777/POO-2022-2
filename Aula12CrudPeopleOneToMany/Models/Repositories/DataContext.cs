425011709dd55b0c8bbf59370327aeec8cd98b7c
Cassio Costa <cassio.hc@gmail.com>
2022-05-20 19:27:38 -0300

    projeto one to many

---
+++ Aula12CrudPeopleOneToMany/Models/Repositories/DataContext.cs
@@ -0,0 +1,16 @@
+using Aula11CrudPeople.Models.Domains;
+using Aula12CrudPeopleOneToMany.Models.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aula11CrudPeople.Models.Repositories
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> opts)
+            :base(opts)
+        {}
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<City> Cities { get; set; }
+    }
+}
---
+++ Aula12CrudPeopleOneToMany/Program.cs
@@ -0,0 +1,32 @@
+using Aula11CrudPeople.Models.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(
+                x=>x.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IPersonRepository,PersonRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
